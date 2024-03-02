d136399ca318527142c95a347a71901bd47a95c8
Niko Dittmar <nikodittmar@gmail.com>
2024-03-01 19:20:14 -0600

    Added login and signup server actions.

--- app/actions/login.ts
+++ app/actions/login.ts
@@ -1,14 +1,42 @@
 'use server'
 
+import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 
 export async function login(prevState: any, formData: FormData) {
+    const body = { 
+        "identifier": formData.get("usernameOrEmail"),
+        "password": formData.get("password")
+    }
 
-    console.log(formData.get("usernameOrEmail"))
-    if (formData.get("password") != "Password123") {
-        return {
-            message: "Incorrect username or password!"
+    const response = await fetch("http://localhost:3001/api/v1" + "/auth/login", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+
+    if (response.ok) {
+        let body = await response.json()
+
+        cookies().set('jwt', body.token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            maxAge: 60 * 60 * 24 * 7, // One week
+            path: '/',
+        })
+        
+        redirect('/')
+    } else {
+        if (response.status == 401) {
+            return {
+                message: "Incorrect username or password."
+            }
+        } else {
+            return {
+                message: "We ran into an unexpected error, please try again later."
+            }
         }
     }
-    redirect('/')
 }
--- app/actions/signup.ts
+++ app/actions/signup.ts
@@ -1,14 +1,47 @@
 'use server'
 
+import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 
 export async function signup(prevState: any, formData: FormData) {
 
-    console.log(formData.get("usernameOrEmail"))
-    if (formData.get("password") != "Password123") {
-        return {
-            message: "Incorrect username or password!"
+    const body = { 
+        "user": { 
+            "username": formData.get("username"), 
+            "email": formData.get("email"), 
+            "password": formData.get("password") 
+        } 
+    }
+
+    const response = await fetch("http://localhost:3001/api/v1" + "/users", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+
+    if (response.ok) {
+        let body = await response.json()
+
+        cookies().set('jwt', body.token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            maxAge: 60 * 60 * 24 * 7, // One week
+            path: '/',
+        })
+        
+        redirect('/')
+    } else {
+        let body = await response.json()
+        if (response.status == 422 && body.hasOwnProperty("errors") && body.errors.length > 0) {
+            return {
+                message: await body.errors[0]
+            }
+        } else {
+            return {
+                message: "We ran into an unexpected error, please try again later."
+            }
         }
     }
-    redirect('/')
 }
