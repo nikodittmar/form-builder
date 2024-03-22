e5105fd2b4120242d71edaa76422917c67781dd4
Niko Dittmar <nikodittmar@gmail.com>
2024-03-22 11:23:28 -0700

    Handled server offline error.

--- app/actions/login.ts
+++ app/actions/login.ts
@@ -8,35 +8,40 @@ export async function login(prevState: any, formData: FormData) {
         "identifier": formData.get("usernameOrEmail"),
         "password": formData.get("password")
     }
-
-    const response = await fetch("http://localhost:3001/api/v1" + "/auth/login", {
-        method: "POST",
-        body: JSON.stringify(body),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-
-    if (response.ok) {
-        let body = await response.json()
-
-        cookies().set('jwt', body.token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            maxAge: 60 * 60 * 24 * 7, // One week
-            path: '/',
+    try {
+        const response = await fetch("http://localhost:3001/api/v1" + "/auth/login", {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json"
+            }
         })
-        
-        redirect('/account')
-    } else {
-        if (response.status == 401) {
-            return {
-                message: "Incorrect username or password."
-            }
+
+        if (response.ok) {
+            let body = await response.json()
+
+            cookies().set('jwt', body.token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                maxAge: 60 * 60 * 24 * 7, // One week
+                path: '/',
+            })
+            
+            redirect('/account')
         } else {
-            return {
-                message: "We ran into an unexpected error, please try again later."
+            if (response.status == 401) {
+                return {
+                    message: "Incorrect username or password."
+                }
+            } else {
+                return {
+                    message: "We ran into an unexpected error, please try again later."
+                }
             }
         }
-    }
+    } catch {
+        return {
+            message: "We ran into an unexpected error, please try again later."
+        }
+    } 
 }
--- app/actions/signup.ts
+++ app/actions/signup.ts
@@ -12,36 +12,41 @@ export async function signup(prevState: any, formData: FormData) {
             "password": formData.get("password") 
         } 
     }
-
-    const response = await fetch("http://localhost:3001/api/v1" + "/users", {
-        method: "POST",
-        body: JSON.stringify(body),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    })
-
-    if (response.ok) {
-        let body = await response.json()
-
-        cookies().set('jwt', body.token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            maxAge: 60 * 60 * 24 * 7, // One week
-            path: '/',
+    try {
+        const response = await fetch("http://localhost:3001/api/v1" + "/users", {
+            method: "POST",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json"
+            }
         })
 
-        redirect('/account')
-    } else {
-        let body = await response.json()
-        if (response.status == 422 && body.hasOwnProperty("errors") && body.errors.length > 0) {
-            return {
-                message: await body.errors[0]
-            }
+        if (response.ok) {
+            let body = await response.json()
+
+            cookies().set('jwt', body.token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                maxAge: 60 * 60 * 24 * 7, // One week
+                path: '/',
+            })
+
+            redirect('/account')
         } else {
-            return {
-                message: "We ran into an unexpected error, please try again later."
+            let body = await response.json()
+            if (response.status == 422 && body.hasOwnProperty("errors") && body.errors.length > 0) {
+                return {
+                    message: await body.errors[0]
+                }
+            } else {
+                return {
+                    message: "We ran into an unexpected error, please try again later."
+                }
             }
+        }
+    } catch {
+        return {
+            message: "We ran into an unexpected error, please try again later."
         }
     }
 }
